28b02443b6aeb7b4f10f55a3cfeba0848a530be8
Nalin Dalal <nalindalal2004@gmail.com>
2026-08-07 12:05:22 +0530

    config

--- packages/db/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-/** Singleton Prisma client used across all backend services for DB access */
-export declare const prismaClient: PrismaClient<import("@prisma/client").Prisma.PrismaClientOptions, never, import("@prisma/client/runtime/library").DefaultArgs>;
-//# sourceMappingURL=index.d.ts.map
